1b0604d0a8a822ffb238ba152c3fe4bfad96dd70
Thomas G Musler <thomas.g.musler@gmail.com>
2021-05-22 09:53:17 -0400

    updated model require path hopefully

--- models/Post.js
+++ models/Post.js
@@ -1,9 +1,33 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const sequelize = require('../config/config');
 
 class Post extends Model {}
 
-
+Post.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      post_body: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      freezeTableName: true,
+      underscored: true,
+      modelName: "post",
+    }
+  );
+  
 
 
 
