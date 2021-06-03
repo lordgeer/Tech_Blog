4c6d4e63852080f1a84d7e39ef6b875a2f1d6b6c
Thomas G Musler <thomas.g.musler@gmail.com>
2021-06-02 23:12:07 -0400

    emergency comment fix

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -38,6 +38,7 @@ router.get('/post/:id', async (req, res) => {
       // serialize the data
       const post = postData.get({ plain: true });
       // which view should we render for a single-post?
+      console.log(JSON.stringify({ post }));
       res.render('single-post', {post});
     } else {
       res.status(404).end();
